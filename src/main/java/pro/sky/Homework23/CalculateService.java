600764ec6ceaaaf40eddd0089423d3a563733b40
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-07-09 19:08:16 +0300

    конец дз

---
+++ src/main/java/pro/sky/Homework23/CalculateService.java
@@ -0,0 +1,19 @@
+package pro.sky.Homework23;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculateService {
+public int plus(int num1, int num2){
+    return num1 + num2;
+}
+    public int minus(int num1, int num2) {
+    return num1 - num2;
+    }
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
